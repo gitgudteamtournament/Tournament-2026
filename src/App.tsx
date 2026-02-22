f960858e94347cf066c89d254bc7a34cdd6b28ee
MartyFiles . <denispahomov01@gmail.com>
2026-02-22 21:46:17 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className="text-2xl text-blue-500">Hello world!</h2>
+    </>
+  )
+}
+
+export default App
